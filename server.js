9265491bdac6188f9560b43460e31e4ad80bcc67
Luke Karrys <lukekarrys@gmail.com>
2021-01-26 10:15:23 -0700

    update coolkit

--- server.js
+++ server.js
@@ -2,28 +2,10 @@ require("dotenv").config()
 const express = require("express")
 const server = require("wicked-coolkit")
 
-const {
-  SALESFORCE_URL = "https://login.salesforce.com",
-  SALESFORCE_AUTH_URL = "https://wickedcoolkit-oauth.herokuapp.com",
-  PORT = 3002,
-  DATABASE_URL,
-  NODE_ENV,
-} = process.env
-
 const { start, app } = server({
-  sf: {
-    loginUrl: SALESFORCE_URL,
-    authUrl: SALESFORCE_AUTH_URL,
-  },
-  pg: {
-    connectionString: DATABASE_URL,
-    ssl: DATABASE_URL.match(/\blocalhost\b/)
-      ? false
-      : { rejectUnauthorized: false },
-  },
-  app: {
-    port: PORT,
-  },
+  loginUrl: process.env.SALESFORCE_URL,
+  authUrl: process.env.SALESFORCE_AUTH_URL,
+  pg: process.env.DATABASE_URL,
 })
 
 app.use(express.static("./public"))
@@ -36,11 +18,11 @@ app.get("/", (req, res) => res.render("trading-card"))
 app.get("/getting-started", (req, res) => res.render("getting-started"))
 
 start()
-  .then(() =>
+  .then(({ port }) =>
     console.log(
       `Server started on ${
-        NODE_ENV === "production" ? "port " : "http://localhost:"
-      }${PORT}`
+        process.env.NODE_ENV === "production" ? "port " : "http://localhost:"
+      }${port}`
     )
   )
   .catch(console.error)
